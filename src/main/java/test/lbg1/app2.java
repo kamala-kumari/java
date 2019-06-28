0537070d4cde330d0c25d3c999c0bc9d7339577b
narasimha <srijakandadi@gmail.com>
2019-06-28 12:26:08 +0530

    this is my first commit message

---
+++ src/main/java/test/lbg1/app2.java
@@ -0,0 +1,15 @@
+package test.lbg1;
+
+public class app2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+	}
+
+	}
+
+
